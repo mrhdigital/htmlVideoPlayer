a5e735838ce7907bd444c62455361eb7ee628cb7
unknown <mrhdigital@yahoo.com>
2019-12-10 19:30:37 -0600

    created scrub function

--- assets/javascript/htmlVideoPlayer.js
+++ assets/javascript/htmlVideoPlayer.js
@@ -49,6 +49,12 @@ function handleProgress(){
    // console.log(percent);
     progressBar.style.flexBasis = `${percent}%`;
 }
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 /* Hook up the event listners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -59,3 +65,4 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click',skip));
 ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
+progress.addEventListener('click', scrub);
